104fb122c48a8a8911369320d456eeee24ce520c
Vlad Slivilescu <vlad.slivilescu00@e-uvt.ro>
2021-11-28 21:12:07 +0200

    [LAB4] Add slight delay to image creation

--- src/com/book_management/Image.java
+++ src/com/book_management/Image.java
@@ -1,10 +1,17 @@
 package com.book_management;
 
+import java.util.concurrent.TimeUnit;
+
 public class Image implements Element {
     private String url;
 
     public Image(String url) {
         this.url = url;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void print() {
